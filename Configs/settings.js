f6e508922d51e88c2e72bb0b11baad3d4574095c
piohio3099jj-tech <piohio3099jj@gmail.com>
2026-01-22 21:00:30 +0400

    Add files via upload

---
+++ Configs/settings.js
@@ -0,0 +1,26 @@
+const settings = {
+Token: process.env.DISCORD_TOKEN,
+    prefix: '$',// البرفكس
+    EmbedColor: '#921f1b',//لون الامبد الي يكون يسار الامبد لازم يكون هيكس
+    BankID:'1438036495838609471',
+    ClientId: '1463612355547103232',
+    
+    ServerInfo: {
+        owner: '1438036495838609471',
+        serverID: '1463689290230796535',
+        Probot: '282859044593598464',
+    },
+    
+    Pic: {
+        Line: 'https://media.discordapp.net/attachments/1441137582917554186/1463701385630716035/line.png?ex=6972c9a4&is=69717824&hm=7ce615594b6861f68202dbe8668950752233ce363fe4e9c02400a82a036b5d46&=&format=webp&quality=lossless&width=2805&height=151',
+        Orders:'https://media.discordapp.net/attachments/1441137582917554186/1463703795409354773/Gemini_Generated_Image_yh0d2myh0d2myh0d-Photoroom.png?ex=6972cbe3&is=69717a63&hm=d68a14eae6e24c64efb3ab125e42d4bced2574a4fada32085382b9f523c47e33&=&format=webp&quality=lossless&width=2016&height=665',
+        Prove: 'https://media.discordapp.net/attachments/1441137582917554186/1463705418093035530/Gemini_Generated_Image_oo9500oo9500oo95-Photoroom.png?ex=6972cd66&is=69717be6&hm=71aacde7af699c887b86331978e725cb95fb0bb8bcbf6e1f55bde732700587b7&=&format=webp&quality=lossless&width=2016&height=636',
+        Ticket:'https://media.discordapp.net/attachments/1441137582917554186/1463705821769891851/Gemini_Generated_Image_gjbvbgjbvbgjbvbg-Photoroom.png?ex=6972cdc6&is=69717c46&hm=87e26c3e5d97f08c3207b811f6a025af54f1c3a1eca2ce440c0e57da9c1b5f88&=&format=webp&quality=lossless&width=2016&height=636',
+        Info: 'https://media.discordapp.net/attachments/1441137582917554186/1463706476542693386/Gemini_Generated_Image_do9mnwdo9mnwdo9m-Photoroom.png?ex=6972ce62&is=69717ce2&hm=a1259bf8307971763e61e1597813a933de64b610963e1f592ebcac40590919fe&=&format=webp&quality=lossless&width=2016&height=636',
+        tashfer: 'https://media.discordapp.net/attachments/1441137582917554186/1463707631859400777/Gemini_Generated_Image_gkfkokgkfkokgkfk-Photoroom.png?ex=6972cf75&is=69717df5&hm=eef57b3b4d8dde579daac2471e2cd753c338681ad0d3d7f4497f59baf732fd83&=&format=webp&quality=lossless&width=2016&height=630',
+        Rules: 'https://media.discordapp.net/attachments/1441137582917554186/1463707841469747394/Gemini_Generated_Image_jjeecdjjeecdjjee-Photoroom.png?ex=6972cfa7&is=69717e27&hm=105c4cb3f1769d3f726be17cdf8eaa6a55d68123a850cd6b2898300405600ae3&=&format=webp&quality=lossless&width=2016&height=625',
+    }
+
+}
+
+module.exports = settings;
